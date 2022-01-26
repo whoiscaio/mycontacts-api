3745b970fb2ce6b4b1334c652e4101abceda9dd6
Caio <whoiscaio@protonmail.com>
2022-01-26 09:30:56 -0300

    feat: first route created

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const server = express();
 
+server.get('/', (request, response) => {
+  response.send('Hello world');
+});
+
 server.listen(3000, () => {
   console.log('Server started at http://localhost:3000');
 })
