7700fbe2e52fb27d1473397ed3c52f770afad595
Caio <whoiscaio@protonmail.com>
2022-01-26 10:15:56 -0300

    feat: put route issue fixed

--- src/routes.js
+++ src/routes.js
@@ -7,7 +7,7 @@ const routes = express.Router();
 routes.get('/contacts', ContactController.index);
 routes.get('/contacts/:id', ContactController.show);
 routes.post('/contacts', ContactController.store);
-routes.put('/contacts', ContactController.update);
+routes.put('/contacts/:id', ContactController.update);
 routes.delete('/contacts/:id', ContactController.delete);
 
 module.exports = routes;
