ce65e99624b1729863a0b47a155bd15e3937d19a
Caio <whoiscaio@protonmail.com>
2022-01-26 10:29:49 -0300

    feat: repository find all func added

--- src/app/repositories/ContactRepository.js
+++ src/app/repositories/ContactRepository.js
@@ -1,6 +1,30 @@
+const { v4: uuid } = require('uuid');
+
+const contacts = [
+  {
+    name: 'Matheus',
+    email: 'matheus@mail.com',
+    phone: '123123123',
+    category_id: uuid(),
+    id: uuid(),
+  }, {
+    name: 'Caio',
+    email: 'caio@mail.com',
+    phone: '321321321',
+    category_id: uuid(),
+    id: uuid(),
+  }, {
+    name: 'Joana',
+    email: 'joana@othermail.com',
+    phone: '231231231',
+    category_id: uuid(),
+    id: uuid(),
+  },
+];
+
 class ContactRepository {
   findAll() {
-
+    return contacts;
   }
 }
 
