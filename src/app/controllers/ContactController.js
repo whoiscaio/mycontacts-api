201a81e00c08a0f86f2b0a77882cd44bba3dc8f0
Caio <whoiscaio@protonmail.com>
2022-01-26 10:11:01 -0300

    feat: contact controller added

---
+++ src/app/controllers/ContactController.js
@@ -0,0 +1,23 @@
+class ContactController {
+  index() {
+    // List all contacts
+  }
+
+  show() {
+    // Get one specific contact
+  }
+
+  store() {
+    // Create a new contact
+  }
+
+  update() {
+    // Update an existing contact
+  }
+
+  delete() {
+    // Delete a contact
+  }
+}
+
+module.exports = new ContactController();
