e4fdd0b1c4604a365d678c04d9d45cdeb0e3da63
Caio <whoiscaio@protonmail.com>
2022-01-27 16:30:40 -0300

    fix: eslint disabled on log line on listen func

--- src/index.js
+++ src/index.js
@@ -6,5 +6,5 @@ app.use(express.json());
 app.use(routes);
 
 app.listen(3000, () => {
-  console.log('Server started at http://localhost:3000');
+  console.log('Server started at http://localhost:3000'); // eslint-disable-line
 });
