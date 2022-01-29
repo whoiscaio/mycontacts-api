c193f835f68dfd9a3537b56f90d9a1f6d3d32e64
Caio <whoiscaio@protonmail.com>
2022-01-29 15:32:03 -0300

    feat: express error handling strategy added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const routes = require('./routes');
+require('express-async-errors');
 
 const app = express();
 app.use(express.json());
 app.use(routes);
+app.use((error, request, response, next) => {
+  response.sendStatus(500);
+});
 
 app.listen(3000, () => {
   console.log('Server started at http://localhost:3000'); // eslint-disable-line
