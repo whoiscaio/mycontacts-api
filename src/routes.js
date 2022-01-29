c3e5c07042050595b276ad410b9922dd23d31cfc
Caio <whoiscaio@protonmail.com>
2022-01-28 23:24:12 -0300

    feat: new file structure

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const ContactController = require('./controllers/ContactController');
+const ContactController = require('./app/controllers/ContactController');
 
 const routes = express.Router();
 
